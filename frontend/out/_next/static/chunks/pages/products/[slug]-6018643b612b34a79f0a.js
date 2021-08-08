229792b9700925a32a0c8b9760a062514a5aa8fd
Tod <ootodoo@gmail.com>
2021-08-08 20:58:36 +0700

    commit

--- frontend/out/_next/static/chunks/pages/products/[slug]-6018643b612b34a79f0a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[905],{9752:function(){},3812:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/products/[slug]",function(){return _(9752)}])}},function(n){n.O(0,[888,774,179],(function(){return u=3812,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
